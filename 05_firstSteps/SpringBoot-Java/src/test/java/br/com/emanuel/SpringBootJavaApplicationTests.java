e5391da006b2065c7503ef9ced024b64ab5e2fa8
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-19 23:22:08 -0300

    Criando Projeto

---
+++ 05_firstSteps/SpringBoot-Java/src/test/java/br/com/emanuel/SpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emanuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
