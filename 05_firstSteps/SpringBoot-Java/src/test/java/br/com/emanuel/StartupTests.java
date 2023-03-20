266644114d7e444002d3e73f1d368de8fd56887c
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-19 23:26:01 -0300

    update

--- 05_firstSteps/SpringBoot-Java/src/test/java/br/com/emanuel/StartupTests.java
+++ 05_firstSteps/SpringBoot-Java/src/test/java/br/com/emanuel/StartupTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootJavaApplicationTests {
+class StartupTests {
 
 	@Test
 	void contextLoads() {
