2bfe121400e0ab6b75f30bb9e4940655c3d4a46a
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-22 00:02:45 -0300

    Implementação da logica da soma

--- Spring-Boot/src/main/java/com/example/demo/MathController.java
+++ Spring-Boot/src/main/java/com/example/demo/MathController.java
@@ -15,11 +15,37 @@ public class MathController {
 	//rest parans = são parametros obrigatorios que, se não serem enviados a aplicacao pode quebrar
 	
 	@GetMapping(value ="/sum/{num1}/{num2}")//esta requerindo valores obrigatorios
-	public Double sum(@PathVariable(value = "num1") Double num1, @PathVariable(value = "num1")Double num2) {
-		return num1 + num2;
+	public Double sum(
+			@PathVariable(value = "num1") String num1, 
+			@PathVariable(value = "num1")String num2
+			) throws Exception	{
+		
+		if(!isNumeric(num1) || !isNumeric(num2)) {
+		throw new Exception();
+		}
+		
+		return convertToDouble(num1) + convertToDouble(num2);
 		 
 	}
+
+	private Double convertToDouble(String strNumber) {
+		
+		if(strNumber == null) 	return 0D;
+		
+		String number = strNumber.replaceAll(",", ".");//vai pegar o que estiver virgula e substituir pelo ponto
+		
+		if(isNumeric(number)) return Double.parseDouble(number);
+		
+		return 1.0D;
+	}
+	 public static boolean isNumeric(String strNumber) {
+	        if (strNumber == null) return false; 
+	        String number = strNumber.replaceAll(",", ".");
+	        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	    }
+	}
+	
+
 	
 	
 	 
-}
