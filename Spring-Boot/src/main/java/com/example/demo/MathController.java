3a574bb6815feaf91a2a94631ed62bd3540a581a
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-22 22:50:37 -0300

    encapsulando metodos e alternadores

--- Spring-Boot/src/main/java/com/example/demo/MathController.java
+++ Spring-Boot/src/main/java/com/example/demo/MathController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.converters.NumberConverter;
 import com.example.demo.exceptions.UnsupportedMathOperationException;
+import com.example.math.SimpleMath;
 
 @RestController
 public class MathController {
@@ -16,82 +18,69 @@ public class MathController {
 	//query parans = são parametros opcionais, não são obrigados a serem enviados
 	//rest parans = são parametros obrigatorios que, se não serem enviados a aplicacao pode quebrar
 	
+	private SimpleMath math = new SimpleMath();
+	
 	@GetMapping(value ="/sum/{num1}/{num2}")//esta requerindo valores obrigatorios
 	public Double sum(
 			@PathVariable(value = "num1") String num1, 
 			@PathVariable(value = "num1")String num2
 			) throws Exception	{
 		
-		if(!isNumeric(num1) || !isNumeric(num2)) {
+				if (!NumberConverter.isNumeric(num1) || !NumberConverter.isNumeric(num2)) {
 		throw new UnsupportedMathOperationException("COLOCA UM NUMERO AE");
 		}
 		
-		return convertToDouble(num1) + convertToDouble(num2);
+		return math.sum(NumberConverter.convertToDouble(num1), NumberConverter.convertToDouble(num2)) ;
 		  
-	}
+	} 
 
 	@GetMapping(value = "/sub/{num1}/{num2}")
 	public Double subtracao(@PathVariable String num1, @PathVariable String num2) throws Exception {
-		if(!isNumeric(num1) || !isNumeric(num2)) throw new UnsupportedMathOperationException("Coloca um numero certo ae");
+		if(!NumberConverter.isNumeric(num1) || !NumberConverter.isNumeric(num2)) throw new UnsupportedMathOperationException("Coloca um numero certo ae");
 		
-		return convertToDouble(num1) - convertToDouble(num2);
-	}
+		return math.subtracao(NumberConverter.convertToDouble(num1), NumberConverter.convertToDouble(num2)) ;	}
 	
 	@GetMapping(value = "/mult/{num1}/{num2}")
 	public Double multiplicacao(@PathVariable String num1,@PathVariable String num2)throws Exception {
 		
-		if(!isNumeric(num1) || !isNumeric(num2)) {
+		if(!NumberConverter.isNumeric(num1) || !NumberConverter.isNumeric(num2)) {
 			throw new UnsupportedMathOperationException("Coloca um num certo ae");
 		}
 		
-		return convertToDouble(num1) * convertToDouble(num2);
+		return math.multiplicacao(NumberConverter.convertToDouble(num1), NumberConverter.convertToDouble(num2)) ;
 	}
 	
 	@GetMapping(value = "/div/{num1}/{num2}")
 	public Double divisao(@PathVariable String num1,@PathVariable String num2) throws Exception {
 		
-		if (!isNumeric(num1) || !isNumeric(num2)) {
+		if (!NumberConverter.isNumeric(num1) || !NumberConverter.isNumeric(num2)) {
 			throw new UnsupportedMathOperationException("num da div errado");
 		}
 		
-		return convertToDouble(num1) / convertToDouble(num2);
+		return math.divisao(NumberConverter.convertToDouble(num1), NumberConverter.convertToDouble(num2)) ;
 	}
 	
 	@GetMapping(value = "/media/{num1}/{num2}")
 	public Double media(@PathVariable String num1,@PathVariable String num2) throws Exception{
 		
-		if (!isNumeric(num1) || !isNumeric(num2)) {
+		if (!NumberConverter.isNumeric(num1) || !NumberConverter.isNumeric(num2)) {
 			throw new UnsupportedMathOperationException("media errada amigao");
 		}
 		
-		return (convertToDouble(num1) + convertToDouble(num2)) /2;
+		return math.media(NumberConverter.convertToDouble(num1), NumberConverter.convertToDouble(num2)) ;
 	}
 	
 	@GetMapping(value = "raiz/{num1}")
 	public Double raiz(@PathVariable String num1) throws Exception{
 		
-		if (!isNumeric(num1)) {
+		if (!NumberConverter.isNumeric(num1)) {
 			throw new UnsupportedMathOperationException("raiz errada meu chapa");
 		}
 		
-		return Math.sqrt(convertToDouble(num1));
+		return math.raiz(NumberConverter.convertToDouble(num1)) ;
 	}
 	
-	private Double convertToDouble(String strNumber) {
-		
-		if(strNumber == null) 	return 0D;
-		
-		String number = strNumber.replaceAll(",", ".");//vai pegar o que estiver virgula e substituir pelo ponto
-		
-		if(isNumeric(number)) return Double.parseDouble(number);
-		
-		return 1.0D;
-	}
-	 public static boolean isNumeric(String strNumber) {
-	        if (strNumber == null) return false; 
-	        String number = strNumber.replaceAll(",", ".");
-	        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-	    }
+	
 	}
 	
 
---
+++ Spring-Boot/src/main/java/com/example/math/SimpleMath.java
@@ -0,0 +1,31 @@
+package com.example.math;
+
+
+public class SimpleMath {
+
+	public Double sum(Double num1, Double num2) {
+		return num1 + num2;
+	}
+
+	public Double subtracao(Double num1, Double num2) {
+		return num1 - num2;
+	}
+
+	public Double multiplicacao(Double num1, Double num2) {
+		return num1 * num2;
+	}
+
+	public Double divisao(Double num1, Double num2) {
+		return num1 / num2;
+	}
+
+	public Double media(Double num1, Double num2) {
+		return (num1 + num2) / 2;
+	}
+
+	public Double raiz(Double num1) {
+
+		return Math.sqrt(num1);
+	}
+
+}
